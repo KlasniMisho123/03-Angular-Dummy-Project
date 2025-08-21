3ab5278fb0ff5d402482d2a4457d03733afb4454
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-08-21 20:03:06 +0400

    components commit

--- src/app/dashboard/tickets/tickets.component.ts
+++ src/app/dashboard/tickets/tickets.component.ts
@@ -14,6 +14,34 @@ interface DummyTrafficDataInterface {
 })
 
 export class TicketsComponent {
-  @Input({required:true}) dummyTrafficData!:DummyTrafficDataInterface;
-  
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
 }
--- src/app/dashboard/trafic/trafic.component.ts
+++ src/app/dashboard/trafic/trafic.component.ts
@@ -8,5 +8,36 @@ import { Component, Input } from '@angular/core';
   styleUrl: './trafic.component.css'
 })
 export class TraficComponent {
-  @Input({required:true}) maxTraffic!:number;
+
+  dummyTrafficData = [
+    {
+      id: 'd1',
+      value: 433,
+    },
+    {
+      id: 'd2',
+      value: 260,
+    },
+    {
+      id: 'd3',
+      value: 290,
+    },
+    {
+      id: 'd4',
+      value: 410,
+    },
+    {
+      id: 'd5',
+      value: 397,
+    },
+    {
+      id: 'd6',
+      value: 488,
+    },
+    {
+      id: 'd47',
+      value: 589,
+    },
+  ];
+  maxTraffic = Math.max(...this.dummyTrafficData.map((data) => data.value));
 }
