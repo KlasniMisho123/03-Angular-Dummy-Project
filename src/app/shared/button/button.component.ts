c1351de521177b2bd21cdb71f1adbe88ca7782f4
Klasnimisho123 <mikheili.silagava.1@btu.edu.ge>
2025-08-28 19:22:47 +0400

    new ticket form

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-button',
+  selector: 'button[appButton]',
   standalone: true,
   imports: [],
   templateUrl: './button.component.html',
